ee0c8c8841098e1511c3a69b8aabffd5141f7796
Desiree Marquez <marquezdecardona@gmail.com>
2020-07-15 19:33:17 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,7 +4,7 @@ def oxford_comma(array)
     elsif array.size == 2
       array.insert(1, "and").join(" ")
       else 
-      array.last.insert(0, "and")
+      array.last.insert(0, "and ")
       array.join(", ")
 end
 end
